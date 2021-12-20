c166dd0098d9be154cf0732bf1397147243010b7
lthuong <33594579+lthuong@users.noreply.github.com>
2021-12-20 22:56:59 +0100

    Simple Sketch
    Simple Sketch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PhotoComponent } from './components/photo/photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FeedComponent,
+    FooterComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule
